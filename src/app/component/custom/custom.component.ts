4cec33443abecd9419046bdea9cad787fa7b56f9
qiao-han <qiao.han@allianz.de>
2023-10-11 14:37:48 +0200

    component custom added

---
+++ src/app/component/custom/custom.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-custom',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './custom.component.html',
+  styleUrls: ['./custom.component.scss']
+})
+export class CustomComponent {
+
+}
