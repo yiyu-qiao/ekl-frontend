836b73fee16eef654a774b75e50f0273e6b3fd7d
qiao-han <qiao.han@allianz.de>
2026-02-09 22:41:41 +0100

    update footer header Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {LoginComponent} from './component/login/login.component';
+import {HeaderComponent} from './component/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Einkaufsliste';
+  currentYear = new Date().getFullYear();
 }
